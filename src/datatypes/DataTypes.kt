e725a91c85162fccc01ca5e60ebf9b950be70887
Gaurav Sharma <gaurav.sharma@go-jek.com>
2020-08-02 01:53:18 +0530

    Adding Kotlin Data Types

--- src/datatypes/DataTypes.kt
+++ src/datatypes/DataTypes.kt
@@ -1,8 +1,21 @@
 package datatypes
 
+import java.lang.StringBuilder
+
 fun main() {
     val myInt = 10
     val myShort: Short = 22
     val myLong = 22L
+    var myDouble = myLong.toInt()
+
+    val char = 'b'
+    val myChar = 65
+    if(myChar is Int){
+        print("\ntrue\n")
+    }
+    val myCharInt = 65
+    println(myCharInt.toChar())
+
+
 
 }
