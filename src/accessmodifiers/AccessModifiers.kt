f53fcfafe55cfdd88cb722be1a34907fab5915b3
Gaurav Sharma <grvsharma@hotmail.com>
2020-08-17 23:54:02 +0530

    Adding Data classes

--- src/accessmodifiers/AccessModifiers.kt
+++ src/accessmodifiers/AccessModifiers.kt
@@ -2,7 +2,7 @@ package accessmodifiers
 
 const val My_CONST = 100
 
-class Employee {
+class Employee(val name: String?) {
 
 }
 
@@ -11,10 +11,11 @@ data class Car(val color: String?, val model: String?, val year: Int?) {
 }
 
 fun main() {
-    val emp = Employee()
-    println(emp)
+    val emp = Employee("John")
+    val emp1 = Employee("John")
+    println(emp == emp1)
 
-    val objects = Car("Red","Ford",2019)
+    val objects = Car("Red", "Ford", 2019)
     println(objects)
 
     println(My_CONST)
